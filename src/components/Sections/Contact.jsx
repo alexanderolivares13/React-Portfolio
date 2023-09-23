cc59381da7c12c9741039ac9b324753edc0297a0
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-09-23 15:39:27 -0700

    completed the contact form with all the styling and validation necessary, the form checks for email validity, and empties the form when the submit button is clicked. The form will also display an error tooltip that mentions that a given field is required if the user exits the field focus and it's empty

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -2,21 +2,41 @@ import { useState } from "react";
 import "./Contact.css";
 
 const Contact = () => {
+  // some regex for email validation, checks for email name, @ and TLD, works for 2 character domain names as well such as .io
+  const emailReg = /^([A-Za-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{1,6})$/;
+
   const [formState, setFormState] = useState({
     email: "",
     name: "",
     message: "",
   });
+  // tooltip state for that checks for email validity, it will show if the email is invalid
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  //state to check for email is valid
+  const [isValid, setIsValid] = useState(true);
+  // state that checks for each respective empty fields, and displays a tooltip if the user exits the input and it's empty
+  const [isEmailEmpty, setIsEmailEmpty] = useState(false);
+  const [isNameEmpty, setIsNameEmpty] = useState(false);
+  const [isMessageEmpty, setIsMessageEmpty] = useState(false);
 
-  //   const [isValid, setIsValid] = useState();
+  // a function that executes after 200ms giving the page enough time to open up the default email app before emptying out the state and fields
+  const clearForm = () => {
+    setTimeout(() => {
+      setFormState({
+        email: "",
+        name: "",
+        message: "",
+      });
+    }, 200);
+  };
 
+  // checks to see if all fields are filled out then opens the default email app with the email pre-filled to match the form
   const submitHandler = (e) => {
     e.preventDefault();
-
-    const mailToLink = `mailto:alexander.olivares1310@gmail.com?subject=NewContact%20<${formState.email}>&body=${formState.message}%0D%0A%0D%0ASincerely,%0D%0A%0D%0A${formState.name}`;
-
-    window.location.href = mailToLink;
-    // setFormState({});
+    if (formState.email && formState.name && formState.message) {
+      const mailToLink = `mailto:alexander.olivares1310@gmail.com?subject=NewContact%20<${formState.email}>&body=${formState.message}%0D%0A%0D%0ASincerely,%0D%0A%0D%0A${formState.name}`;
+      window.location.href = mailToLink;
+    }
   };
 
   return (
@@ -24,42 +44,81 @@ const Contact = () => {
       <h2 className='contact-me'>Contact Me:</h2>
       <form action='submit' className='form' onSubmit={submitHandler}>
         <div>
-          <label>Email:</label>
+          <label>Email*:</label>
           <input
             type='email'
-            name='email'
+            value={formState.email}
             id='email'
-            onChange={(e) =>
-              setFormState({ ...formState, email: e.target.value })
+            onFocus={() => setTooltipVisible(true)}
+            onBlur={() =>
+              !isEmailEmpty && formState.email === ""
+                ? setIsEmailEmpty(true)
+                : setIsEmailEmpty(false)
             }
+            onChange={(e) => {
+              setFormState({ ...formState, email: e.target.value });
+              emailReg.test(formState.email)
+                ? setIsValid(true)
+                : setIsValid(false);
+              setIsEmailEmpty(false);
+            }}
             placeholder='something@example.com'
-          ></input>
+          />
+          {isValid === false && tooltipVisible && (
+            <div
+              style={{
+                color: "red",
+              }}
+            >
+              {isValid ? "" : "Invalid Email"}
+            </div>
+          )}
+          {isEmailEmpty && <div style={{ color: "red" }}>Required Field</div>}
         </div>
         <div>
-          <label>Name:</label>
+          <label>Name*:</label>
           <input
             type='text'
-            name='name'
+            value={formState.name}
             id='name'
-            onChange={(e) =>
-              setFormState({ ...formState, name: e.target.value })
+            onBlur={() =>
+              !isNameEmpty && formState.name === ""
+                ? setIsNameEmpty(true)
+                : setIsNameEmpty(false)
             }
+            onChange={(e) => {
+              setFormState({ ...formState, name: e.target.value });
+              setIsNameEmpty(false);
+            }}
             placeholder='John Doe'
-          ></input>
+          />
+          {isNameEmpty && <div style={{ color: "red" }}>Required Field</div>}
         </div>
         <div>
-          <label>Message:</label>
+          <label>Message*:</label>
           <textarea
             type='text'
-            name='message'
+            value={formState.message}
             id='message'
-            onChange={(e) =>
-              setFormState({ ...formState, message: e.target.value })
+            onBlur={() =>
+              !isMessageEmpty && formState.message === ""
+                ? setIsMessageEmpty(true)
+                : setIsMessageEmpty(false)
             }
+            onChange={(e) => {
+              setFormState({ ...formState, message: e.target.value });
+              setIsMessageEmpty(false);
+            }}
             placeholder='Hello! I love your work!'
           ></textarea>
+          <p>* Required fields</p>
+          {isMessageEmpty && (
+            <div style={{ color: "red", marginBottom: "20px" }}>
+              Required Field
+            </div>
+          )}
         </div>
-        <button>Submit</button>
+        <button onClick={clearForm}>Submit</button>
       </form>
     </div>
   );
