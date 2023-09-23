dbe91cee1a5fb912f762b5300c901f8f3a9826b4
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-09-23 15:55:24 -0700

    added a heading and a message within the projects component.

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -4,6 +4,10 @@ import Card from "../Card/Card";
 const Projects = () => {
   return (
     <div className='content-cards'>
+      <h2>Projects:</h2>
+      <div style={{ width: "100%" }}>
+        *Click on the image to be taken to the deployed site
+      </div>
       {projectsArray.map((projects) => (
         <Card key={projects.id} projectsArray={projects} />
       ))}
